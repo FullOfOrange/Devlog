3807a05d13a54fee8b102b09770242813894e102
FullOfOrange <foorgdev@gmail.com>
2020-01-19 01:44:50 +0900

    init: divide client & server cause expand problems

--- bin/www.mjs
+++
@@ -1,9 +0,0 @@
-import "../src/env.mjs";
-import { app, client } from "../src/app.mjs";
-
-const { APP_PORT } = process.env;
-
-(async function() {
-  await client.prepare();
-  app.listen(APP_PORT);
-})();
