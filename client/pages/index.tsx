6b46b1477d4cd5518f62d7a6f71a51dd8967313e
FullOfOrange <foorgdev@gmail.com>
2020-04-10 23:09:37 +0900

    feat: Client 초기 환경 설정

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,12 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from "next";
+
+const Home: NextPage = () => (
+  <h1>Hello world! - user agent</h1>
+);
+
+// 초기 화면 그릴 때 사용됨, NextPage 에서 받아서 사용함
+Home.getInitialProps = async ({ req }) => {
+  return;
+};
 
 export default Home;
