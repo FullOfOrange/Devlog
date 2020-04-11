635932510df30cf5b89d2945e299feea7766c7b3
FullOfOrange <foorgdev@gmail.com>
2020-04-11 23:51:54 +0900

    feat: 전체 포스팅 불러오는 것에 대한 라우터 추가

--- server/router/router.go
+++ server/router/router.go
@@ -1,14 +1,16 @@
 package router
 
 import(
-	"github.com/gin-gonic/gin"
+"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	route := gin.New()
 	route.Use(gin.Recovery())
 
-//	apiv1 := route.Group("/api/v1")
+	apiv1 := route.Group("/api/v1")
+
+	apiv1.GET("/posts", v1.GetPosts)
 
 	return route
 }
