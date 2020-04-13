e8a9edb8cf8f0293b9140f0171eaf38d2c1158d5
FullOfOrange <foorgdev@gmail.com>
2020-04-14 01:46:36 +0900

    feat: 홈 화면 추가

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,41 @@
 import { NextPage } from "next";
+import Card from "../components/Card";
+import {Post} from "../types"
 
-const Home: NextPage = () => (
-  <h1>Hello world! - user agent</h1>
-);
+interface initPosts {
+  posts: Array<Post>;
+}
+
+const Home: NextPage<initPosts> = ({ posts }) => {
+  return (
+    <div>
+      {posts.map(post => {
+        return (
+          <Card
+            post={post}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 // 초기 화면 그릴 때 사용됨, NextPage 에서 받아서 사용함
-Home.getInitialProps = async ({ req }) => {
-  return;
+Home.getInitialProps = async () => {
+  return {
+    posts: [
+      {
+        title: "안녕",
+        author: "안녕",
+        contents: "방가방가"
+      },
+      {
+        title: "안녕",
+        author: "안녕",
+        contents: "방가방가"
+      }
+    ]
+  };
 };
 
 export default Home;
