a3c589b3911182839e1043a65849c997c9f4f6f3
FullOfOrange <foorgdev@gmail.com>
2020-05-10 19:27:56 +0900

    refactor: 카드 컴포넌트 레이아웃 일부 수정

--- client/components/Card/index.tsx
+++ client/components/Card/index.tsx
@@ -7,13 +7,15 @@ const Card = ({ post }: { post: Post }): React.ReactElement => {
   const date = new Date(post.createAt);
 
   return (
-    <S.Card>
-      <S.Title>{post.title}</S.Title>
-      <S.Contents>{post.contents}</S.Contents>
-      <S.Date>{`${date.getFullYear()}년 ${date.getMonth() +
-        1}월 ${date.getDate()}일`}</S.Date>
-      <S.Author>by {post.author}</S.Author>
-    </S.Card>
+    <>
+      <S.Card>
+        <S.Title>{post.title}</S.Title>
+        <S.Contents>{post.contents}</S.Contents>
+        <S.Date>{`${date.getFullYear()}년 ${date.getMonth() +
+          1}월 ${date.getDate()}일`}</S.Date>
+        <S.Author>by {post.author}</S.Author>
+      </S.Card>
+    </>
   );
 };
 export default Card;
